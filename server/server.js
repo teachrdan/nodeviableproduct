eeb53f396d791a977e787d00df52bb3b0980fad3
Dan Tennery-Spalding <teachrdan@gmail.com>
2017-05-30 16:26:00 -0700

    added get/post/put/delete

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 var _ = require('lodash');
 var express = require('express');
 var app = express();
-
 app.use(express.static('client'));
+
+app.get('/', function(req, res) {
+  
+});
+
+app.post('/', function(req, res) {
+
+});
+
+app.put('/', function(req, res) {
+
+});
+
+app.delete('/', function(req, res) {
+
+});
+
 app.use(function(err, req, res, next) {
   if (err) {
-    res.status(500).send(error);
+    res.status(500).send('Error.');
   }
 });
 
